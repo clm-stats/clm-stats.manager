182bd95f18b3f0756a7e1879055b2fdfc44b2035
Mitch Dzugan <mitchdzugan@gmail.com>
2026-05-06 18:43:53 -0500

    better scripting tools

--- scripts/build.js
+++ scripts/build.js
@@ -1,12 +1,27 @@
+import commandLineArgs from "command-line-args";
 import build from "#lib/build";
-/*
 import * as A from "#lib/action";
 
-const gamesUnknown = { w: "W", l: "L" };
-*/
+const optionDefinitions = [
+  { name: "add-event", alias: "e", lazyMultiple: true, type: String },
+  { name: "bust-cache", alias: "b", lazyMultiple: true, type: String },
+  { name: "stop-fetching", alias: "s", lazyMultiple: true, type: String },
+];
+
+const options = commandLineArgs(optionDefinitions);
 
 async function main() {
-  await build.startBuild();
+  const newActions = Object.entries(options).flatMap(([opt, vals]) => {
+    if (opt === "add-event") {
+      return vals.map((slug) => A.AddEvent(slug));
+    } else if (opt === "bust-cache") {
+      return vals.map((slug) => A.BustGQLCacheForEvent(slug));
+    } else if (opt === "stop-fetching") {
+      return vals.map((slug) => A.SetDoneUpdating(slug));
+    }
+    return [];
+  });
+  await build.startBuild(newActions);
 }
 
 main()
