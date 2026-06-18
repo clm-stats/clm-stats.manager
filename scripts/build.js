f6994e4ba213229b8ebb49a8245a0b9d597a3e43
Mitch Dzugan <mitchdzugan@gmail.com>
2026-06-18 16:06:10 -0500

    add challonge support

--- scripts/build.js
+++ scripts/build.js
@@ -6,6 +6,9 @@ const optionDefinitions = [
   { name: "add-event", alias: "e", lazyMultiple: true, type: String },
   { name: "bust-cache", alias: "b", lazyMultiple: true, type: String },
   { name: "stop-fetching", alias: "s", lazyMultiple: true, type: String },
+  { name: "set-event-mask", alias: "m", lazyMultiple: true, type: String },
+  { name: "mark-challonge", alias: "c", lazyMultiple: true, type: String },
+  { name: "name-override", alias: "n", lazyMultiple: true, type: String },
   { name: "set-current-period", alias: "p", type: Number },
 ];
 
@@ -15,8 +18,20 @@ async function main() {
   const newActions = Object.entries(options).flatMap(([opt, vals]) => {
     if (opt === "add-event") {
       return vals.map((slug) => A.AddEvent(slug));
+    } else if (opt === "mark-challonge") {
+      return vals.map((slug) => A.MarkChallonge(slug));
+    } else if (opt === "name-override") {
+      // `${SLUG}:${NAME}`
+      return vals.map((args) => {
+        const [slug, ...restArgs] = args.split(":");
+        const name = restArgs.join(":");
+        return A.OverrideName(slug, name);
+      });
     } else if (opt === "bust-cache") {
       return vals.map((slug) => A.BustGQLCacheForEvent(slug));
+    } else if (opt === "set-event-mask") {
+      // `${EVENT_PARTICIPANT_ID}:${MASKED_IDENT}:${SLUG}`
+      return vals.map((args) => A.IdForEvent(...args.split(":")));
     } else if (opt === "stop-fetching") {
       return vals.map((slug) => A.SetDoneUpdating(slug));
     } else if (opt === "set-current-period") {
